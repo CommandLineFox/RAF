51ef30096b7af031fe72ae7cdb471fceca3216cd
AlexArcticFox <alextromb1@gmail.com>
2022-03-15 17:16:06 +0100

    Echo fix

--- src/commands/Administration/Echo.ts
+++ src/commands/Administration/Echo.ts
@@ -4,9 +4,16 @@ import type { CommandInteraction } from "discord.js";
 export default class Echo extends Command {
     public constructor() {
         super("echo", "Ponavlja poruku xd");
+        this.data.addStringOption(option =>
+            option.setName("message")
+                .setDescription("Message for repeating")
+                .setRequired(true)
+        )
     }
 
     async execute(interaction: CommandInteraction): Promise<void> {
-        interaction.reply("Pong xd");
+        const message = interaction.options.getString("message", true);
+        interaction.channel?.send(message);
+        interaction.reply({ content: "Послато", ephemeral: true });
     }
 }
